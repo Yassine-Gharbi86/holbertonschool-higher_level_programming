eb68d61104c57b2cefe1eff30296608d003fbce8
root <root@DESKTOP-NDVQC7F.localdomain>
2025-01-17 18:35:46 +0100

    2-print_alphabet.py

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-def print_alphabet(x):
-    if x <= 122:
-        print(chr(x), end="")
-        print_alphabet(x + 1)
-
-
-print_alphabet(97)
+for x in range(97, 123):
+    print(chr(x), end="")
