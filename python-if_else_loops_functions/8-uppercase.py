1de5c8ed93ebddeb4e9877cd119fce9d292ac195
root <root@DESKTOP-NDVQC7F.localdomain>
2025-01-18 14:31:27 +0100

    8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    x =""
+    x = ""
     for char in str:
         if ord(char) in range(97, 123):
-            y=chr(ord(char) - 32)
+            y = chr(ord(char) - 32)
             x = x + y
         else:
             x = x + char
     print(x)
-  
