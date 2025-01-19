2e16af3320fb4b80c083f942788620eb055e9c54
root <root@DESKTOP-NDVQC7F.localdomain>
2025-01-19 14:39:31 +0100

    12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for x in range(1, 101):
-        if x % 5 == 0 and x % 3 == 0: 
+        if x % 5 == 0 and x % 3 == 0:
             print("FizzBuzz", end=" ")
         elif x % 3 == 0:
-            print("Fizz", end= " ")
+            print("Fizz", end=" ")
         elif x % 5 == 0:
-            print("Buzz", end= " ")
+            print("Buzz", end=" ")
         else:
-            print (x, end=" ")
+            print(x, end=" ")
