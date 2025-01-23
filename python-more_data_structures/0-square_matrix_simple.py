c8a9b2a6d41b9e1968b95ce1549f9c7975e7077c
root <root@DESKTOP-NDVQC7F.localdomain>
2025-01-23 15:48:40 +0100

    0-square_matrix_simple.py

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    x = []
+    for i in matrix:
+        y = []
+        for j in i:
+           p = (j * j)
+           y.append(p)
+        x.append(y)
+    return x
