f81df86be4f8f24ec3a34d3ebd31c53b5702cbe0
root <root@DESKTOP-NDVQC7F.localdomain>
2025-01-25 12:44:10 +0100

    4-list_division.py

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -9,7 +9,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         except TypeError:
             print("wrong type")
-            resut = 0
+            result = 0
         except IndexError:
             print("out of range")
             result = 0
