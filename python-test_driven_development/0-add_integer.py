33d0ebf93ebd5a75cb95ca7a5e450a2e849d7ae6
root <root@DESKTOP-NDVQC7F.localdomain>
2025-01-26 12:35:14 +0100

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -41,4 +41,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    return round(a) + round(b)
