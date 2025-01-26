348cd7af6cf9c0c33bfed8af0d5b11ecd8f58c43
root <root@DESKTOP-NDVQC7F.localdomain>
2025-01-26 15:53:22 +0100

    6-max_integer_test.py

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,6 +5,7 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
+
 class TestMaxInteger(unittest.TestCase):
 
     def test_max_at_end(self):
@@ -12,6 +13,9 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_max_in_middle(self):
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+
+    def test_max_at_beginning(self):
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)  # Adding this case
 
     def test_single_element(self):
         self.assertEqual(max_integer([7]), 7)
@@ -24,6 +28,7 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_mixed_numbers(self):
         self.assertEqual(max_integer([-3, 4, 2, -1]), 4)
+
 
 
 if __name__ == '__main__':
