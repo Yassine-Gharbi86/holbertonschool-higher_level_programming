20eb7023f7847d4ac7647795668f6992cf583b8b
root <root@DESKTOP-NDVQC7F.localdomain>
2025-03-09 21:32:56 +0100

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,40 +1,28 @@
 #!/usr/bin/python3
 """
-Script to list all states from the database `hbtn_0e_0_usa`.
+Lists all states from the database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./0-select_states.py <mysql username>\
-            <mysql password> <database name>")
-        sys.exit(1)
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=db_name
-        )
-    except MySQLdb.Error as e:
-        print(f"Error connecting to MySQL: {e}")
-        sys.exit(1)
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
     cursor = db.cursor()
-    try:
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    except MySQLdb.Error as e:
-        print(f"Error executing query: {e}")
-        db.close()
-        sys.exit(1)
-    rows = cursor.fetchall()
-    for row in rows:
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cursor.fetchall():
         print(row)
+
     cursor.close()
     db.close()
