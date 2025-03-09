2415588dd96a852d7410953aac277702c0cae5b6
root <root@DESKTOP-NDVQC7F.localdomain>
2025-03-09 20:47:08 +0100

    tasks

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,6 @@
--- Creates the table force_name.
-CREATE TABLE IF NOT EXISTS `force_name` (
-    `id`   INT,
-    `name` VARCHAR(256) NOT NULL
+-- Script to create the table force_name with constraints
+
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
 );
--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,6 @@
--- Creates the table id_not_null
-CREATE TABLE IF NOT EXISTS `id_not_null` (
-    `id`   INT          DEFAULT 1,
-    `name` VARCHAR(256)
+-- Script to create the table unique_id with constraints
+
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256)
 );
